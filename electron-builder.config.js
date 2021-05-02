217b29d143702af2fc4263699a5ab95b516259a1
parsehex <1779937+parsehex@users.noreply.github.com>
2021-05-02 17:07:09 -0400

    test

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -1,21 +1,31 @@
-const now = new Date;
-const buildVersion = `${now.getFullYear() - 2000}.${now.getMonth() + 1}.${now.getDate()}`;
+const now = new Date();
+const buildVersion = `${now.getFullYear() - 2000}.${
+	now.getMonth() + 1
+}.${now.getDate()}`;
 
 /**
  * @type {import('electron-builder').Configuration}
  * @see https://www.electron.build/configuration/configuration
  */
 const config = {
-  directories: {
-    output: 'dist',
-    buildResources: 'buildResources',
-  },
-  files: [
-    'packages/**/dist/**',
-  ],
-  extraMetadata: {
-    version: buildVersion,
-  },
+	appId: 'net.thomasmays.dashboard',
+	productName: 'Dashboard',
+	copyright: 'Copyright © 2021 Thomas Mays',
+	directories: {
+		output: 'dist',
+		buildResources: 'buildResources',
+	},
+	win: {
+		target: 'nsis',
+	},
+	files: ['packages/**/dist/**'],
+	extraMetadata: {
+		version: buildVersion,
+	},
+	publish: {
+		provider: 'github',
+		private: true,
+	},
 };
 
 module.exports = config;
